03809c7046708b1c3277f7e0788d4b4c6e695627
sporting-mutu <sporting-mutu@ssh3>
2017-11-08 13:05:34 +0100

    JS test

--- src/assets/js/lib/demosite.js
+++ src/assets/js/lib/demosite.js
@@ -1,12 +1,14 @@
 import $ from 'jquery';
 
-$.ajax({
-  dataType: 'jsonp',
-  url: 'https://api.github.com/repos/olefredrik/foundationpress?callback=foundationpressGithub&access_token=ed6229228dbc763038dbf1e68d0d8a4a0935b38a',
-  success: function (response) {
-    if (response && response.data.watchers) {
-      var watchers = (Math.round((response.data.watchers / 100), 10) / 10).toFixed(1);
-      $('#stargazers a').html(watchers + 'k stargazers');
-    }
-  }
+
+jQuery('.orbit').on('beforeslidechange.zf.orbit', function(){
+  console.log('in');
+  var nextImage = jQuery('.orbit-slide.is-active + .orbit-slide img.orbit-image'),
+      orbit = jQuery('.orbit');
+  nextImage.css({
+    position: 'absolute',
+    right: '-30%',
+    top: nextImage.position().top - orbit.position().top
+  });
 });
+
